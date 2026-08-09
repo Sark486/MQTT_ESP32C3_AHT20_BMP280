61b700564adbc47a82bc5134d73aa415a189adfe
Oleksandr Romanchenko <agressog@gmail.com>
2026-08-09 19:48:44 +0300

    Oversample and filter the BMP280 to steady the pressure reading

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -40,6 +40,13 @@ bool beginBmp() {
     return false;
   }
   Serial.println(F("[sensors] BMP280 found"));
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
   bmpPressure = bmp.getPressureSensor();
   return bmpPressure != nullptr;
 }
